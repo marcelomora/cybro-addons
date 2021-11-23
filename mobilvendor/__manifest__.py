e807be87530dc87193fa7c5d6f59995928e8a9db
marcelomora <java.diablo@gmail.com>
2021-11-23 09:14:31 -0500

    [14.0][ADD] Mobilvendor modules

---
+++ mobilvendor/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "MobilVendor",
+    "summary": "MobilVendor integration",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "accioma.com",
+    "author": " Accioma",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "data": [
+        "views/account_journal_views.xml",
+    ],
+    "depends": [
+        "sale", "account"
+    ],
+
+
+}
+
+
---
+++ product_family/__manifest__.py
@@ -0,0 +1,37 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    'name': 'Product Family',
+    'version': '14.0.1.0.1',
+    'category': "Generic Modules",
+    'description': """ This module adds product family field""",
+    'author': 'AvanzOSC',
+    'website': 'http://www.openerp.com',
+    'depends': ['base', 'stock'],
+    'init_xml': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_family_view.xml',
+        'views/product_template_view.xml'
+                   ],
+    'demo_xml': [],
+    'installable': True,
+    'auto_install': False,
+}
